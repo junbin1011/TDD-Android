d1872878fd8c4fb5b03f5172a148560d59b0a24e
qinyu <qinyusuain@gmail.com>
2020-06-15 11:03:24 +0800

    init commit

---
+++ app/src/main/java/com/cac/app/NewsActivity.kt
@@ -0,0 +1,14 @@
+package com.cac.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewsActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
+
